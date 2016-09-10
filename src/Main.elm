209eb3f24c6c3fa3d156e4f858830a24c42b5e0f
Eason Chen <easoncxz@gmail.com>
2016-09-10 04:12:12 -0700

    Support "rm task" feature

--- src/Main.elm
+++ src/Main.elm
@@ -25,6 +25,7 @@ type Msg
     = NoOp
     | UpdateNewTaskTitleField String
     | SubmitNewTask
+    | DeleteTaskAt Int
 
 -- Model
 
@@ -35,6 +36,9 @@ model =
     }
 
 -- Update
+
+splitAt : Int -> List a -> (List a, List a)
+splitAt n xs = (List.take n xs, List.drop n xs)
 
 update : Msg -> Model -> Model
 update msg model =
@@ -51,15 +55,38 @@ update msg model =
                 , status = NeedsAction
                 }]
             }
+        DeleteTaskAt n ->
+            if n < 0 then
+                model
+            else
+                { model
+                | tasks =
+                    let (front, back) = splitAt n model.tasks
+                    in case List.tail back of
+                        Just bs ->
+                            front ++ bs
+                        Nothing ->
+                            front
+                }
+
 
 -- View
 
 showTask : ZTask -> H.Html Msg
 showTask t = H.p [] [ H.text t.title ]
 
+showTaskWithCancel : Int -> ZTask -> H.Html Msg
+showTaskWithCancel n t =
+    H.div []
+        [ H.span [] [ H.text t.title ]
+        , H.button
+            [ HE.onClick (DeleteTaskAt n) ]
+            [ H.text "rm" ]
+        ]
+
 showTaskList : List ZTask -> H.Html Msg
 showTaskList ts =
-    H.div [] (List.map showTask ts)
+    H.div [] (List.indexedMap showTaskWithCancel ts)
 
 newTaskPrompt : Model -> H.Html Msg
 newTaskPrompt model =

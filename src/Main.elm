3438cb897346affda0227ce220f1a25154316f79
Eason Chen <easoncxz@gmail.com>
2016-09-10 03:46:33 -0700

    Make add-to-list functionality!
    - `onSubmit` should've been `onClick`.
    - Forget about Google Tasks for now.
    - Forget about non-aliasing types for now.

--- src/Main.elm
+++ src/Main.elm
@@ -5,142 +5,81 @@ import Html.Attributes as HT
 import Html.Events as HE
 import Date exposing (Date(..))
 
--- Model
-
 type Id a = Id String
-
-type Tree a
-    = Leaf a
-    | Branches (List (Tree a))
 
 type TaskStatus
     = NeedsAction
     | Completed
 
-type TaskPosition = TaskPosition String
-
-type TaskListKindValue = TaskListKindValue String
-taskListKindValue = TaskListKindValue "tasks#taskList"
-
-type LinkEntities = LinkEntities
-    { type'       : String
-    , description : String
-    , link        : String
+type alias ZTask =
+    { title  : String
+    , status : TaskStatus
     }
 
-type GTask = GTask
-    { kind      : TaskListKindValue
-    , id        : Id GTask
-    -- , etag      : String
-    , title     : String
-    -- , updated   : Date
-    -- , selfLink  : Id GTask
-    , parent    : Maybe (Id GTask)
-    , position  : TaskPosition
-    -- , notes     : String
-    , status    : TaskStatus
-    -- , due       : Date
-    -- , completed : Date
-    , deleted   : Bool
-    , hidden    : Bool
-    -- , links     : List LinkEntities
-    }
-
-type GTaskList = GTaskList
-    { kind     : TaskListKindValue
-    , id       : Id GTaskList
-    , etag     : String
-    , title    : String
-    , updated  : Date
-    , selfLink : String
-    }
-
-type Model = Model
-    { accessToken       : String
-    , tasks             : Tree GTask
-    , inputValue        : String
+type alias Model =
+    { tasks             : List ZTask
     , newTaskTitleField : String
     }
 
+type Msg
+    = NoOp
+    | UpdateNewTaskTitleField String
+    | SubmitNewTask
+
+-- Model
+
 model : Model
-model = Model
-    { accessToken = "(no token)"
-    , tasks = Branches []
-    , inputValue = "initial value"
+model =
+    { tasks             = []
     , newTaskTitleField = "New task"
     }
 
 -- Update
 
-type Msg
-    = NoOp
-    | UpdateInput String
-    | UpdateNewTaskTitleField String
-    | SubmitNewTask
-
-makeDefaultTask : String -> GTask
-makeDefaultTask title = GTask
-    { kind     = taskListKindValue
-    , id       = Id "new id"  -- TODO
-    , title    = title
-    , parent   = Nothing
-    , position = TaskPosition ""  -- TODO
-    , status   = NeedsAction
-    , deleted  = False
-    , hidden   = False
-    }
-
 update : Msg -> Model -> Model
-update msg (Model model) =
+update msg model =
     case msg of
         NoOp ->
-            Model model
-        UpdateInput t ->
-            Model { model | inputValue = t }
+            model
         UpdateNewTaskTitleField t ->
-            Model { model | newTaskTitleField = t }
+            { model | newTaskTitleField = t }
         SubmitNewTask ->
-            Model { model
-                | newTaskTitleField = ""
-                , tasks =
-                    let newTask = makeDefaultTask model.newTaskTitleField
-                    in case model.tasks of
-                        Leaf task ->
-                            Branches [Leaf task, Leaf newTask]
-                        Branches taskList ->
-                            Branches (taskList ++ [Leaf newTask])
-                }
+            { model
+            | newTaskTitleField = ""
+            , tasks = model.tasks ++ [
+                { title = model.newTaskTitleField
+                , status = NeedsAction
+                }]
+            }
 
 -- View
 
-showTaskTree : Tree GTask -> H.Html Msg
-showTaskTree tree =
-    case tree of
-        Leaf (GTask task) ->
-            H.p [] [ H.text task.title ]
-        Branches trees ->
-            H.div [] <| List.map showTaskTree trees
+showTask : ZTask -> H.Html Msg
+showTask t = H.p [] [ H.text t.title ]
 
-newTaskSubmitter : Model -> H.Html Msg
-newTaskSubmitter (Model m) =
+showTaskList : List ZTask -> H.Html Msg
+showTaskList ts =
+    H.div [] (List.map showTask ts)
+
+newTaskPrompt : Model -> H.Html Msg
+newTaskPrompt model =
     H.div []
         [ H.input
-            [ HE.onInput UpdateNewTaskTitleField
-            , HT.value m.newTaskTitleField
-            ]
-            []
+            [ HT.value model.newTaskTitleField
+            , HE.onInput UpdateNewTaskTitleField
+            ] []
         , H.input
             [ HT.type' "submit"
-            , HE.onSubmit SubmitNewTask ]
-            []
+            , HE.onClick SubmitNewTask
+            ] []
         ]
 
 view : Model -> H.Html Msg
-view (Model model) =
+view model =
     H.div []
         [ H.h1 [] [ H.text "OGTC client" ]
-        , showTaskTree model.tasks
-        , newTaskSubmitter (Model model)
+        , showTaskList model.tasks
+        , newTaskPrompt model
         ]
 
 main =
